a43a254e0143491ebb3e7fbf6647deae7d6d1a47
anjula-sack <anjulashanaka@gmail.com>
2023-06-27 21:10:38 +0530

    Add theme tests

--- src/pages/Footer.test.js
+++ src/pages/Footer.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
+import { BrowserRouter } from "react-router-dom";
 import Footer from "./Footer";
 
 const mockStore = configureStore([]);
@@ -20,9 +21,11 @@ describe("Footer", () => {
 
   it("renders correctly", () => {
     render(
-      <Provider store={store}>
-        <Footer />
-      </Provider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <Footer />
+        </Provider>
+      </BrowserRouter>
     );
   });
 });
