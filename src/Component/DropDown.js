1f1fe684c54bfbdee2922869378616b6c19f51f5
sanjana0319 <asanjana0823@gmail.com>
2023-09-29 11:42:47 +0530

    Improve the code formatting of the dropdown component

--- src/Component/DropDown.js
+++ src/Component/DropDown.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import Select from 'react-select';
- 
+import React from "react";
+import Select from "react-select";
 
- const DropDown =  (props) => {
-return(
-<Select
-        value={props.selectedOption}
-        onChange={props.onChange}
-        options={props.options}
-      />
-)  
-
-}
-export default DropDown
+const DropDown = (props) => {
+  return (
+    <Select
+      value={props.selectedOption}
+      onChange={props.onChange}
+      options={props.options}
+    />
+  );
+};
+export default DropDown;
