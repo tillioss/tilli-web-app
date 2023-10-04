c2784b12df18c85ae530939fca461a41d89daf06
Sanjana <126409127+Sanjana0319@users.noreply.github.com>
2023-10-04 19:00:46 +0530

    Remove the commented-out code snippets in the DoubleBoxOverlapWithImage.js (#83)
    * Improve the code formatting of the dropdown component
    
    * Remove the commented-out code snippets

--- src/Component/Themes/DoubleBoxOverlapWithImage.js
+++ src/Component/Themes/DoubleBoxOverlapWithImage.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React from "react";
 import { Style } from "react-style-tag";
-import backImage from '../../images/outlineBackIcon.png';
-import nextImage from '../../images/outlineRightIcon.png';
-import MyConstant from '../../config/MyConstant';
+import backImage from "../../images/outlineBackIcon.png";
+import nextImage from "../../images/outlineRightIcon.png";
+import MyConstant from "../../config/MyConstant";
 import { Link } from "react-router-dom";
 
-
 class DoubleBoxOverlapWithImage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
-    render() {
-        let { stage, data } = this.props;
-        let content = data.content;
-        console.log(window.innerWidth);
-        return (
-            <React.Fragment>
-                <Style>
-                    {`
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    let { stage, data } = this.props;
+    let content = data.content;
+    console.log(window.innerWidth);
+    return (
+      <React.Fragment>
+        <Style>
+          {`
                     .center {
                         display: flex;
                         justify-content: center;
@@ -48,71 +45,110 @@ class DoubleBoxOverlapWithImage extends React.Component {
                         z-Index:1;
                       }
                       `}
-                </Style>
-                <div className="module-content">
-                    <div className="col-12">
-                        <div className="row mt-4">
-                            <div className="col-2">
-                                <Link onClick={() => this.props.changeStage('Previous', stage)}>
-                                    <img style={{ width: 48, height: 48 }} src={backImage} alt={""} />
-                                </Link>
-                            </div>
-                            <div className="col-10" style={{ alignSelf: 'center' }}>
-                                <p style={{
-                                    //color: '#474A57',
-                                    fontSize: 27,
-                                    fontFamily: 'montserrat-extrabold',
-                                    fontWeight: '800',
-                                    alignSelf: 'center',
-                                }}>
-                                    {data.title}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{
-                        marginTop: window.innerHeight / 10,
-                    }}>
-                        <div style={{
-                            marginLeft: '5%',
-                            marginRight: '5%',
-                        }}>
-                            <div className="pink" style={{ position: 'absolute', minHeight: window.innerHeight / 2.1, }} >
-                                <p className="font-24-18"
-                                    style={{
-                                        //color: '#474A57',
-                                        fontFamily: 'montserrat-medium',
-                                        fontWeight: '600',
-                                        margin: 0, paddingLeft: 10, paddingRight: 10, textAlign: 'center'
-                                    }}>
-                                    {content.text}
-                                </p>
-                            </div>
-                        </div>
-                        <div style={{
-                            marginLeft: '7%',
-                            marginRight: '7%',
-                        }}>
-                            <div className="yellow" style={{ position: 'absolute', minHeight: window.innerHeight / 2 }} >
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{ position: 'absolute', bottom: '17%', left: '5%', zIndex: 3 }} >
-                        <img style={{ width: window.innerHeight / 5 }} src={MyConstant.keyList.apiURL +
-                            'vp?action=module&key=' +
-                            content.image.fileName +
-                            '&id=' +
-                            content.image.fileType} alt={""} />
-                    </div>
-                </div>
-                <div className="forward-step">
-                    <Link onClick={() => this.props.changeStage('Next', stage)}>
-                        <img style={{ width: 44, height: 44 }} src={nextImage} alt={""} />
-                    </Link>
-                </div>
-            </React.Fragment >
-        )
-    }
+        </Style>
+        <div className="module-content">
+          <div className="col-12">
+            <div className="row mt-4">
+              <div className="col-2">
+                <Link onClick={() => this.props.changeStage("Previous", stage)}>
+                  <img
+                    style={{ width: 48, height: 48 }}
+                    src={backImage}
+                    alt={""}
+                  />
+                </Link>
+              </div>
+              <div className="col-10" style={{ alignSelf: "center" }}>
+                <p
+                  style={{
+                    fontSize: 27,
+                    fontFamily: "montserrat-extrabold",
+                    fontWeight: "800",
+                    alignSelf: "center",
+                  }}
+                >
+                  {data.title}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              marginTop: window.innerHeight / 10,
+            }}
+          >
+            <div
+              style={{
+                marginLeft: "5%",
+                marginRight: "5%",
+              }}
+            >
+              <div
+                className="pink"
+                style={{
+                  position: "absolute",
+                  minHeight: window.innerHeight / 2.1,
+                }}
+              >
+                <p
+                  className="font-24-18"
+                  style={{
+                    fontFamily: "montserrat-medium",
+                    fontWeight: "600",
+                    margin: 0,
+                    paddingLeft: 10,
+                    paddingRight: 10,
+                    textAlign: "center",
+                  }}
+                >
+                  {content.text}
+                </p>
+              </div>
+            </div>
+            <div
+              style={{
+                marginLeft: "7%",
+                marginRight: "7%",
+              }}
+            >
+              <div
+                className="yellow"
+                style={{
+                  position: "absolute",
+                  minHeight: window.innerHeight / 2,
+                }}
+              ></div>
+            </div>
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              bottom: "17%",
+              left: "5%",
+              zIndex: 3,
+            }}
+          >
+            <img
+              style={{ width: window.innerHeight / 5 }}
+              src={
+                MyConstant.keyList.apiURL +
+                "vp?action=module&key=" +
+                content.image.fileName +
+                "&id=" +
+                content.image.fileType
+              }
+              alt={""}
+            />
+          </div>
+        </div>
+        <div className="forward-step">
+          <Link onClick={() => this.props.changeStage("Next", stage)}>
+            <img style={{ width: 44, height: 44 }} src={nextImage} alt={""} />
+          </Link>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default DoubleBoxOverlapWithImage
+export default DoubleBoxOverlapWithImage;
