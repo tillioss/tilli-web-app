a45a2774b5765f1fe9e5e52e83ea70a9a82e491d
Madhawa Monarawila <70215958+Madhawa97@users.noreply.github.com>
2023-10-31 21:14:04 +0530

    Remove comments from SideMenu.js and TopMenu.js (#109)

--- src/Screens/Menu/TopMenu.js
+++ src/Screens/Menu/TopMenu.js
@@ -35,16 +35,11 @@ class TopMenu extends React.Component
                       <span class="badge bg-red pull-right">50%</span>
                       <span>Settings</span>
                     </a>
-                {/* <a class="dropdown-item"  href="javascript:;">Help</a> */}
                   <a class="dropdown-item"  href={"/"+MyConstant.keyList.projectUrl}><i class="fa fa-sign-out pull-right"></i> Log Out</a>
                 </div>
               </li>
 
               <li role="presentation" class="nav-item dropdown open">
-                {/* <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false">
-                  <i class="fa fa-envelope-o"></i>
-                  <span class="badge bg-green">6</span>
-                </a> */}
                 <ul class="dropdown-menu list-unstyled msg_list" role="menu" aria-labelledby="navbarDropdown1">
                   <li class="nav-item">
                     <a class="dropdown-item">
